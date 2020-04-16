45d94ebd27b54078c304ecc8e2909cdc9d87e00d
Andrew Ben Richard <andrewbenrichard@gmail.com>
2020-04-16 13:00:36 +0100

    Update front_extras.blade.php

--- resources/views/layouts/front_extras.blade.php
+++ resources/views/layouts/front_extras.blade.php
@@ -1,48 +1,48 @@
-<script src="/js/front_app.js"></script>
+<script src="{{ URL::asset('js/front_app.js') }}"></script>
   <!-- Jquery Slim JS -->
-    <script src="/assets/js/jquery.min.js"></script>
+    <script src="{{ URL::asset('assets/js/jquery.min.js') }}"></script>
     <!-- Popper JS -->
-    <script src="/assets/js/popper.min.js"></script>
+    <script src="{{ URL::asset('assets/js/popper.min.js') }}"></script>
     <!-- Bootstrap JS -->
-    <script src="/assets/js/bootstrap.min.js"></script>
+    <script src="{{ URL::asset('assets/js/bootstrap.min.js') }}"></script>
     <!-- Meanmenu JS -->
-    <script src="/assets/js/jquery.meanmenu.js"></script>
+    <script src="{{ URL::asset('assets/js/jquery.meanmenu.js') }}"></script>
     <!-- Easing Min JS -->
-    <script src="/assets/js/jquery.easing.min.js"></script>
+    <script src="{{ URL::asset('assets/js/jquery.easing.min.js') }}"></script>
     <!-- Owl Carousel JS -->
-    <script src="/assets/js/owl.carousel.js"></script>
+    <script src="{{ URL::asset('assets/js/owl.carousel.js') }}"></script>
     <!-- Owl Magnific JS -->
-    <script src="/assets/js/jquery.magnific-popup.min.js"></script>
+    <script src="{{ URL::asset('assets/js/jquery.magnific-popup.min.js') }}"></script>
     <!-- Nice Select JS -->
-    <script src="/assets/js/jquery.nice-select.min.js"></script>
+    <script src="{{ URL::asset('assets/js/jquery.nice-select.min.js') }}"></script>
     <!-- Appear JS -->
-    <script src="/assets/js/jquery.appear.js"></script>
+    <script src="{{ URL::asset('assets/js/jquery.appear.js') }}"></script>
     <!-- Odometer JS -->
-    <script src="/assets/js/odometer.min.js"></script>
+    <script src="{{ URL::asset('assets/js/odometer.min.js') }}"></script>
     <!-- Image Light Box JS -->
-    <script src="/assets/js/imagelightbox.min.js"></script>
+    <script src="{{ URL::asset('assets/js/imagelightbox.min.js') }}"></script>
     <!-- Form Ajaxchimp JS -->
-    <script src="/assets/js/jquery.ajaxchimp.min.js"></script>
+    <script src="{{ URL::asset('assets/js/jquery.ajaxchimp.min.js') }}"></script>
     <!-- Form Validator JS -->
-    <script src="/assets/js/form-validator.min.js"></script>
+    <script src="{{ URL::asset('assets/js/form-validator.min.js') }}"></script>
     <!-- Contact JS -->
-    <script src="/assets/js/contact-form-script.js"></script>
+    <script src="{{ URL::asset('assets/js/contact-form-script.js') }}"></script>
     <!-- Custom JS -->
-    <script src="/assets/js/main.js"></script>
+    <script src="{{ URL::asset('assets/js/main.js') }}"></script>
 
     <!-- Slider Revolution core JavaScript files -->
-    <script src="/assets/revolution/js/jquery.themepunch.tools.min.js"></script>
-    <script src="/assets/revolution/js/jquery.themepunch.revolution.min.js"></script>
-    <script src="/assets/revolution/js/extensions/revolution.extension.actions.min.js"></script>
-    <script src="/assets/revolution/js/extensions/revolution.extension.carousel.min.js"></script>
-    <script src="/assets/revolution/js/extensions/revolution.extension.kenburn.min.js"></script>
-    <script src="/assets/revolution/js/extensions/revolution.extension.layeranimation.min.js"></script>
-    <script src="/assets/revolution/js/extensions/revolution.extension.migration.min.js"></script>
-    <script src="/assets/revolution/js/extensions/revolution.extension.navigation.min.js"></script>
-    <script src="/assets/revolution/js/extensions/revolution.extension.parallax.min.js"></script>
-    <script src="/assets/revolution/js/extensions/revolution.extension.slideanims.min.js"></script>
-    <script src="/assets/revolution/js/extensions/revolution.extension.video.min.js"></script>
-    <script src="/assets/js/rev-slider-custom.js"></script>
+    <script src="{{ URL::asset('assets/revolution/js/jquery.themepunch.tools.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/revolution/js/jquery.themepunch.revolution.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/revolution/js/extensions/revolution.extension.actions.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/revolution/js/extensions/revolution.extension.carousel.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/revolution/js/extensions/revolution.extension.kenburn.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/revolution/js/extensions/revolution.extension.layeranimation.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/revolution/js/extensions/revolution.extension.migration.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/revolution/js/extensions/revolution.extension.navigation.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/revolution/js/extensions/revolution.extension.parallax.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/revolution/js/extensions/revolution.extension.slideanims.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/revolution/js/extensions/revolution.extension.video.min.js') }}"></script>
+    <script src="{{ URL::asset('assets/js/rev-slider-custom.js') }}"></script>
 <script>
     $( document ).ready(function() {
         console.log( "ready!" );
