154fe5bcc15e087c74e2af13db85edbc21d90523
Andrew Ben Richard <andrewbenrichard@gmail.com>
2020-06-16 19:25:38 +0100

    js

--- public/js/front_app.js
+++ public/js/front_app.js
@@ -2278,6 +2278,41 @@ __webpack_require__.r(__webpack_exports__);
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 
 /* harmony default export */ __webpack_exports__["default"] = ({
